c58341b3febc9d42e3315861b6c617fbeded234d
kakaye <ian.kyalo@students.jkuat.ac.ke>
2019-12-23 07:40:25 -0500

    Setup test class completely

--- src/QuickFindTestClass.java
+++ src/QuickFindTestClass.java
@@ -1,5 +1,9 @@
 public class QuickFindTestClass {
     public static void main(String args[]){
 
+        QuickFind quickFind = new QuickFind(3);
+
+        quickFind.union(1,2);
+        if (quickFind.isConnected(1,2)) System.out.println("1 and 2 is connected");
     }
 }
